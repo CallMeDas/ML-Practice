a168296a1767680d95ce74ad9ac952ce24591897
callmedas <deepakkdas77@gmail.com>
2026-03-17 20:18:52 +0545

    learing the concept

---
+++ gradient_algo/gradient_algo.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def gradient_descent(x,y):
+    m_curr = b_curr = 0
+    iterations = 100
+    n = len(x)
+    learning_rate = 0.0003
+    for i in range(iterations):
+        y_predict = m_curr *x +b_curr
+        cost = (1/n) * sum([val **2  for val in (y - y_predict)])
+        md = -(2/n) * sum(x * (y - y_predict))
+        bd = -(2/n) * sum(y - y_predict)
+        m_curr = m_curr - learning_rate * md
+        b_curr = b_curr - learning_rate * bd
+        print('m_curr {} b_curr {} cost {} iterations {}'.format(m_curr, b_curr, cost, i))
+    
+
+x = np.array([92, 56, 88, 70, 80, 49, 65, 35, 66, 67])
+y = np.array([98, 68, 81, 80, 83, 52, 66, 30, 68, 73])
+
+gradient_descent(x,y)
